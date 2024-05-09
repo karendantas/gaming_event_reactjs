687c055c9bfc681a8de8205f6f22ae4a68f79de3
Karen Dantas <karennobre74@gmail.com>
2024-05-08 21:12:20 -0300

    style: set global styles with styled components and add favicon

---
+++ src/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const  GlobalStyle = createGlobalStyle`
+:root{
+font-family: 'Roboto', sans-serif;
+    line-height: 1.6;
+    font-weight: 400;
+
+    --white-100: #f9fafb;
+    --gray-100: #e9e7e7;
+    --purple-400: #976aff;
+    --purple-500: #8358e7;
+    --red-400: #ef4444;
+    --yellow-500: #ffef5e;
+    --brown-700: #292524;
+    background-color: var(--purple-400);
+
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+
+`
