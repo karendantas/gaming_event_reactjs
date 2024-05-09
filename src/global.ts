228a712ba5d67f72e5a82389048e25d002454990
Karen Dantas <karennobre74@gmail.com>
2024-05-08 21:50:05 -0300

    feat: add forms

--- src/global.ts
+++ src/global.ts
@@ -21,4 +21,14 @@ font-family: 'Roboto', sans-serif;
     -moz-osx-font-smoothing: grayscale;
 }
 
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+
+::placeholder{
+    color: var(--brown-700)
+}
+
 `
