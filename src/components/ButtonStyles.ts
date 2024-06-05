67e359dd72b80a9eacb552a3cccc6b02ef2beb4d
Karen Dantas <karennobre74@gmail.com>
2024-06-05 18:17:55 -0300

    refactor: create default theme to set app colors

--- src/components/ButtonStyles.ts
+++ src/components/ButtonStyles.ts
@@ -5,16 +5,18 @@ export const ButtonContainer = styled.div`
     z-index: 99;
     bottom: 3rem;
 `;
+
+
 export const Button = styled.button`
-    border: 4px solid var(--brown-700);
+    border: 4px solid ${props => props.theme.colors["brown-700"]};
     width: 10rem;
     height: 4rem;
     border-radius: 5px;
     font-weight: bold;
 
-    background-color:  var(--purple-400);
+    background-color: ${props => props.theme.colors["purple-400"]};
     font-size: 1.25rem;
     color: var(--white-100);
 
-    :hover{ background-color:  var(--purple-500);  }
+    :hover{ background-color:  ${props => props.theme.colors["purple-500"]};  }
 `;
